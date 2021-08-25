4e7aa86867f3a53d009705b5b6f9d27ff5a207c8
Gimsongsam <gimsong83@gmail.com>
2021-08-25 16:44:59 +0900

    MongoDB를 연동한 REST API 개발 예제 실습

--- src/api/posts/index.js
+++ src/api/posts/index.js
@@ -3,20 +3,11 @@ import * as postsCtrl from './posts.ctrl';
 
 const posts = new Router();
 
-// const printInfo = ctx => {
-//     ctx.body = {
-//         method: ctx.method,
-//         path: ctx.path,
-//         params: ctx.params,
-//     };
-// };
-
 // (라우트 설정, printInfo 함수 호출)
 posts.get('/', postsCtrl.list);
 posts.post('/', postsCtrl.write);
 posts.get('/:id', postsCtrl.read);
 posts.delete('/:id', postsCtrl.remove);
-posts.put('/:id', postsCtrl.replace);
 posts.patch('/:id', postsCtrl.update);
 
 export default posts;
