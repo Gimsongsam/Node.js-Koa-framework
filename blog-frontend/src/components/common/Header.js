295a017803c05af954cac751f24e9868a3199aa1
Gimsongsam <gimsong83@gmail.com>
2021-09-22 16:01:28 +0900

    글쓰기 UI 구현하기 예제 실습

--- blog-frontend/src/components/common/Header.js
+++ blog-frontend/src/components/common/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import Responsive from '../Responsive'
+import Responsive from './Responsive'
 import Button from './Button';
 import {Link} from 'react-router-dom'
 
--- blog-frontend/src/pages/WritePage.js
+++ blog-frontend/src/pages/WritePage.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import styled from 'styled-components';
+import Editor from '../components/write/Editor';
+import Responsive from '../components/common/Responsive';
+import TagBox from '../components/write/TagBox';
+import WriteActionButtons from '../components/write/WriteActionButton';
+
+const Content = styled(Responsive)`
+    padding-bottom: 0.25rem;
+`
 
 const WritePage = () => {
-    return <div>글쓰기</div>;
+    return (
+        <Content>
+            <Editor />
+            <TagBox />
+            <WriteActionButtons />
+        </Content>
+    );
 };
 
 export default WritePage;
