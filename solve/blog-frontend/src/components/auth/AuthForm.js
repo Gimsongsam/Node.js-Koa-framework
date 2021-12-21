d6092899c695eb83cc2d317a0348570c8afd13a4
Gimsongsam <gimsong83@gmail.com>
2021-12-21 18:51:54 +0900

    리덕스로 폼 상태 관리하기

--- solve/blog-frontend/src/components/auth/AuthForm.js
+++ solve/blog-frontend/src/components/auth/AuthForm.js
@@ -60,20 +60,28 @@ const textMap = {
     register: '회원가입',
 };
 
-const AuthForm = ({type}) => {
+const AuthForm = ({type, form, onChange, onSubmit}) => {
     const text = textMap[type];
     // 받아온 파라미터로 객체에 접근하기 위해서는 대괄호 참조 방식을 사용해야한다.
 
     return (
         <AuthFormBlock>
             <h3>{text}</h3>
-            <form>
-                <StyledInput autoComplete="username" name="username" placeholder="아이디" />
+            <form onSubmit={onSubmit}>
+                <StyledInput 
+                    autoComplete="username" 
+                    name="username" 
+                    placeholder="아이디" 
+                    onChange={onChange}
+                    value={form.username}
+                />
                 <StyledInput 
                     authComplete="new-password"
                     name="password"
                     placeholder="비밀번호"
                     type="password"
+                    onChange={onChange}
+                    value={form.password}
                 />
                 {type === 'register' && (
                     <StyledInput 
@@ -81,6 +89,8 @@ const AuthForm = ({type}) => {
                         name="passwordConfirm"
                         placeholder="비밀번호 확인"
                         type="password"
+                        onChange={onChange}
+                        value={form.passwordConfirm}
                     />
                 )}
                 <ButtonWithMarginTop cyan fullWidth>
